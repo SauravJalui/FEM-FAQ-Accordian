66eea745e9b1e6a91be8047a38250b562a90b6c5
Saurav Jalui <saurav.jalui-codes@outlook.com>
2022-07-28 18:36:01 +0530

    FEM-FAQ Accordian: add js

---
+++ js/script.js
@@ -0,0 +1,9 @@
+const item = document.querySelectorAll(".item");
+
+for (let i = 0; i < item.length; i++) {
+
+  item[i].addEventListener("click", function () {
+    // alert("item clicked")
+    this.classList.toggle("open");
+  });
+}
